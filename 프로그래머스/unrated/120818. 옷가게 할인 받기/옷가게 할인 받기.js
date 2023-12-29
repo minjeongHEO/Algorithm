ff2fec60ac21044ac07883a457392f988cafc9d1
minjeongHEO <96780693+minjeongHEO@users.noreply.github.com>
2023-12-29 22:23:15 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,13 @@
+function solution(price) {
+    var answer = price;
+
+    if (price >= 500000) {
+      answer *= 0.8;
+    }else if (price >= 300000) {
+      answer *= 0.9;
+    }else if (price >= 100000) {
+      answer *= 0.95;
+    }
+
+    return Math.floor(answer);
+}
