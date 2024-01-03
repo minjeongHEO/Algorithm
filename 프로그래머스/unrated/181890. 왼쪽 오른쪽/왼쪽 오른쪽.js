2cf6d35fe57060282dd9a1477c484966a9e38c70
minjeongHEO <96780693+minjeongHEO@users.noreply.github.com>
2024-01-03 23:34:50 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,13 +1,23 @@
 function solution(str_list) {
-  let result = []
-  for (const [idx, char] of str_list.entries()) {
-    if (char === "l") {
-      result = str_list.slice(0, idx)
-      break
-    } else if (char === "r") {
-      result = str_list.slice(idx + 1)
-      break
-    }
+  // let result = []
+  // for (const [idx, char] of str_list.entries()) {
+  //   if (char === "l") {
+  //     result = str_list.slice(0, idx)
+  //     break
+  //   } else if (char === "r") {
+  //     result = str_list.slice(idx + 1)
+  //     break
+  //   }
+  // }
+  // return result
+    
+  const locL = str_list.indexOf("l")
+  const locR = str_list.indexOf("r")
+
+  if ((locL < locR || locR === -1) && locL !== -1) {
+    return str_list.slice(0, locL)
+  } else if ((locL > locR || locL === -1) && locR !== -1) {
+    return str_list.slice(locR+1)
   }
-  return result
+  return [] // "l"이나 "r"이 없는 경우
 }
