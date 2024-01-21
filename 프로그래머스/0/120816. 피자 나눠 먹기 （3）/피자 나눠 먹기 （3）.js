e83bff5e9cac397a4859994c557477295387b8d1
minjeongHEO <96780693+minjeongHEO@users.noreply.github.com>
2024-01-21 21:43:34 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,10 +1,14 @@
 function solution(slice, n) {
-    var answer = 0;
-    
-    answer = Math.floor(n/slice);
-    
-    if(n%slice != 0){
-        answer += 1
-    }
-    return answer;
+  var answer = 0
+
+  answer = Math.floor(n / slice)
+
+  if (n % slice != 0) {
+    answer += 1
+  }
+  return answer
 }
+
+// function solution(slice, n) {
+//     return Math.ceil(n / slice)
+// }
