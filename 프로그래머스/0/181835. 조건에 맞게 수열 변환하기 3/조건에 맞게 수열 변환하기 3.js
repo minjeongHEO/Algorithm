9fcc2f205a83618eca73bc0dbc3aae6ac95c5e47
minjeongHEO <96780693+minjeongHEO@users.noreply.github.com>
2024-02-04 23:14:13 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 2.68 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -0,0 +1,15 @@
+function solution(arr, k) {
+
+  var answer = []
+
+  if (k % 2 == 0) {
+    answer = arr.map(e => {
+      return e + k
+    })
+  } else {
+    answer = arr.map(e => {
+      return e * k
+    })
+  }
+  return answer
+}
