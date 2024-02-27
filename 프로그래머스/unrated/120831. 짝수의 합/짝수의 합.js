ede98008b1d16d2bdadb9ffb22f3463d033728b1
minjeongHEO <hmjoung9770@gmail.com>
2024-02-27 17:16:22 +0900

    refactor: reduce() 사용

--- 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.js
@@ -1,12 +1,15 @@
 function solution(n) {
-    var answer = 0;
-    
-    while (n > 0) {
-        if(n%2 == 0){
-            answer += n
-        }
-        n--;
-    }
-    
-    return answer;
+  var answer = 0;
+
+  answer = new Array({ length: n })
+    .map((_, idx) => {
+      return idx + 1;
+    })
+    .reduce((acc, cur) => {
+      if (cur % 2 == 0) acc + cur;
+    }, 0);
+
+  return answer;
 }
+
+solution(10);
