44187634de7d0e997d27678e167f3426983261a6
minjeongHEO <hmjoung9770@gmail.com>
2024-08-21 00:21:05 +0900

    style: 시간초과났던 코드 작성

--- 백준/Silver/10828. 스택/스택.js
+++ 백준/Silver/10828. 스택/스택.js
@@ -34,3 +34,57 @@ const main = input => {
 
     console.log(result.join("\n"));
 };
+
+// 시간초과났던 코드
+/*
+
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+let count = 0;
+
+readline
+    .on("line", function (line) {
+        if (count === 0) {
+            count = parseInt(line);
+        } else {
+            input.push(line); // 입력받은 문자열 형 변환
+            if (input.length === count) {
+                readline.close(); // 입력 종료
+            }
+        }
+    })
+    .on("close", function () {
+        main(input); // 문제 풀이 함수 호출
+        process.exit(); // 프로세스 종료
+    });
+
+const runPop = stack => (stack.length ? [true, stack[stack.length - 1], stack.slice(0, stack.length - 1)] : [false, -1]);
+const runPush = (stack, e) => [...stack, parseInt(e.split(" ")[1])];
+const runTop = stack => (stack.length ? stack[stack.length - 1] : -1);
+const runSize = stack => stack.length;
+const runEmpty = stack => (stack.length ? 0 : 1);
+
+const main = input => {
+    let stack = [];
+
+    input.forEach(e => {
+        if (e.includes("push")) stack = runPush(stack, e);
+        if (e === "top") console.log(runTop(stack));
+        if (e === "size") console.log(runSize(stack));
+        if (e === "empty") console.log(runEmpty(stack));
+        if (e === "pop") {
+            const popResult = runPop(stack);
+            if (popResult[0]) {
+                console.log(popResult[1]);
+                stack = popResult[2];
+            } else {
+                console.log(-1);
+            }
+        }
+    });
+};
+*/
