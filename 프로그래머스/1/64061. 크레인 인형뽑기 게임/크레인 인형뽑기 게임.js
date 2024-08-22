c2a035cc280a40b1de058bbe69e81debdaa417e5
Woody <96780693+minjeongHEO@users.noreply.github.com>
2024-08-22 20:54:27 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.57 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,31 +1,21 @@
 const solution = (board, moves) => {
-    let box = Array.from({ length: board.length }).map(e => Array.from(0));
+    const newBoard = Array.from({ length: board.length }).map(e => Array(0));
+    board.reverse().forEach(i => i.forEach((j, idx) => j !== 0 && newBoard[idx].push(j)));
+
+    const stack = [];
     let count = 0;
-    let baskets = [];
 
-    board.reverse().forEach(i => {
-        i.forEach((j, idx) => {
-            if (j) box[idx].push(j);
-        });
-    });
+    moves.forEach(move => {
+        const pop = newBoard[move - 1].pop();
 
-    moves.forEach(e => {
-        const pick = box[e - 1].pop();
-        if (pick) baskets.push(pick);
+        if (pop) stack.push(pop);
 
-        let isPang = true;
-        while (isPang) {
-            isPang = false;
-            for (let i = 0; i < baskets.length - 1; i++) {
-                if (baskets[i] === baskets[i + 1]) {
-                    baskets.splice(i, 2);
-                    count += 2;
-                    isPang = true;
-                    break;
-                }
-            }
+        if (stack.length - 2 >= 0 && stack[stack.length - 2] === pop) {
+            stack.pop();
+            stack.pop();
+            count += 2;
         }
     });
+
     return count;
 };
-
