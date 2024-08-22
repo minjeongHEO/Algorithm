021437aa1b47b06b6422968f7f49067b4ac596f7
minjeongHEO <hmjoung9770@gmail.com>
2024-08-22 14:30:47 +0900

    refactor: 다른 풀이

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,4 +1,10 @@
 function solution(phone_number) {
-    var answer = '';
-    return answer;
+    const e = Array(phone_number.length - 4).fill("*");
+    const n = phone_number.split("").slice(-4);
+    return [...e, ...n].join("");
+}
+
+// 다른 풀이: 정규식 사용
+function hide_numbers(s) {
+    return s.replace(/\d(?=\d{4})/g, "*");
 }
