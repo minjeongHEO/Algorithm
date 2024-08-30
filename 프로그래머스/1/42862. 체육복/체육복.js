ff917b9cb1e45bfaf1e3624578a74ab69785542c
Woody <96780693+minjeongHEO@users.noreply.github.com>
2024-08-30 17:22:09 +0900

    [level 1] Title: 체육복, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,25 +1,16 @@
 function solution(n, lost, reserve) {
-    let reserve2 = reserve.filter(e => !lost.includes(e)).sort((a,b)=>a-b);
-    const lost2 = lost.filter(e => !reserve.includes(e)).sort((a,b)=>a-b);
+    let reserveSet = new Set(reserve.filter(r => !lost.includes(r)));
+    let lostSet = Array.from(new Set(lost.filter(l => !reserve.includes(l)))).sort((a, b) => a - b);
 
-    const result = lost2
-        .map(e => {
-            const idxPlus1 = reserve2.indexOf(e + 1);
-            const idxMinus1 = reserve2.indexOf(e - 1);
-            
-            if (idxMinus1 !== -1) {
-                reserve2.splice(idxMinus1, 1);
-                return;
-            } 
-            if (idxPlus1 !== -1) {
-                reserve2.splice(idxPlus1, 1);
-                return;
-            }
-            return e;
-        })
-        .filter(e => e !== undefined);
+    lostSet.forEach(l => {
+        if (reserveSet.has(l - 1)) {
+            reserveSet.delete(l - 1);
+        } else if (reserveSet.has(l + 1)) {
+            reserveSet.delete(l + 1);
+        } else {
+            n--;
+        }
+    });
 
-    return n - result.length;
+    return n;
 }
-
-
