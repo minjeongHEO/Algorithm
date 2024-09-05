69bcbe3531ff0fc4c65420d07d0fe05b29892fc1
Woody <96780693+minjeongHEO@users.noreply.github.com>
2024-09-05 15:52:55 +0900

    [level 2] Title: 주식가격, Time: 29.91 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,31 +1,11 @@
-// function solution(prices) {
-//     return prices.map((price, idx) => {
-//         const arr = prices.slice(idx);
-//         const pivot = arr.findIndex(e => e < price);
-//         if (pivot === -1) return arr.length - 1;
-//         return pivot;
-//     });
-// }
 function solution(prices) {
-    const answer = new Array(prices.length).fill(0);
-    const stack = [];
-    
-    for (let i = 0; i < prices.length; i++) {
-        // 스택에 있는 인덱스들 중에서 현재 가격보다 높은 가격을 가진 인덱스를 처리
-        while (stack.length && prices[stack[stack.length - 1]] > prices[i]) {
-            const j = stack.pop();
-            answer[j] = i - j;
+    const len = prices.length;
+    return prices.map((p, i) => {
+        let count = 0;
+        for (let j = 0; j < len - (i + 1); j++) {
+            count += 1;
+            if (prices[i + j + 1] - p < 0) break;
         }
-        stack.push(i);
-    }
-
-    // 스택에 남아있는 인덱스들은 끝까지 가격이 떨어지지 않은 경우
-    while (stack.length) {
-        const j = stack.pop();
-        answer[j] = prices.length - 1 - j;
-    }
-    
-    return answer;
+        return count;
+    });
 }
-
-
