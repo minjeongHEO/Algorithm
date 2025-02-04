f37c17a2bfc3dd73a2c4014ed90cd4232a04e899
Woody <96780693+minjeongHEO@users.noreply.github.com>
2025-02-04 13:52:12 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.44 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,53 +1,25 @@
-// const solution = (board, moves) => {
-//     let box = Array.from({ length: board.length }).map(e => Array.from(0));
-//     let count = 0;
-//     let baskets = [];
-
-//     board.reverse().forEach(i => {
-//         i.forEach((j, idx) => {
-//             if (j) box[idx].push(j);
-//         });
-//     });
-
-//     moves.forEach(e => {
-//         const pick = box[e - 1].pop();
-//         if (pick) baskets.push(pick);
-
-//         let isPang = true;
-//         while (isPang) {
-//             isPang = false;
-//             for (let i = 0; i < baskets.length - 1; i++) {
-//                 if (baskets[i] === baskets[i + 1]) {
-//                     baskets.splice(i, 2);
-//                     count += 2;
-//                     isPang = true;
-//                     break;
-//                 }
-//             }
-//         }
-//     });
-//     return count;
-// };
-
-// 다시 풀기
-const solution = (board, moves) => {
-    const newBoard = Array.from({ length: board.length }).map(e => Array(0));
-    board.reverse().forEach(i => i.forEach((j, idx) => j !== 0 && newBoard[idx].push(j)));
-
-    const stack = [];
-    let count = 0;
-
-    moves.forEach(move => {
-        const pop = newBoard[move - 1].pop();
-
-        if (pop) stack.push(pop);
-
-        if (stack.length - 2 >= 0 && stack[stack.length - 2] === pop) {
-            stack.pop();
-            stack.pop();
-            count += 2;
-        }
+function solution(board, moves) {
+  const stacks = Array.from({ length: board.length }).map((e) => []);
+  const bucket = [];
+  let result = 0;
+  board.reverse().forEach((e) => {
+    e.forEach((a, idx) => {
+      if (a) stacks[idx].push(a);
     });
+  });
 
-    return count;
-};
+  moves.forEach((m) => {
+    const popItem = stacks[m - 1].pop();
+
+    if (popItem) {
+      if (bucket[bucket.length - 1] === popItem) {
+        result += 2;
+        bucket.pop();
+      } else {
+        bucket.push(popItem);
+      }
+    }
+  });
+
+  return result;
+}
