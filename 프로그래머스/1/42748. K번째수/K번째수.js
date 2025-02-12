8c120737a0fb480dc682b305e02f843ad787335d
Woody <96780693+minjeongHEO@users.noreply.github.com>
2025-02-12 14:12:47 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,5 +1,7 @@
 function solution(array, commands) {
-    return commands.map(e => {
-        return array.slice(e[0] - 1, e[1]).sort((a, b) => a - b)[e[2] - 1];
-    });
+    return commands.map((e)=>{
+        const k = e[2]-1;
+        return array.slice(e[0]-1, e[1]).sort((a,b)=> a-b)[k];
+
+    })
 }
