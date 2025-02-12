44130910d7600322d1b716db47476d01eb528bdf
Woody <96780693+minjeongHEO@users.noreply.github.com>
2025-02-12 23:24:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.51 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,18 +1,31 @@
 function solution(participant, completion) {
-    const list = new Map();
+    // const result = new Map();
+    
+//     participant.forEach(e=>{
+//         if(result.has(e)) result.set(e, result.get(e) + 1)
+//         else result.set(e, 1)
+//     })
 
-    participant.forEach(e => {
-        if (!list.get(e)) list.set(e, 1);
-        else list.set(e, list.get(e) + 1);
-    });
-
-    completion.forEach(e => {
-        list.set(e, list.get(e) - 1);
-    });
-
-    for (const [key, value] of list.entries()) {
-        if (value > 0) return key;
-    }
-
-    // return participant.filter(e=> !completion.includes(e))[0]
+//     completion.forEach(e=>{
+//         result.set(e, result.get(e) - 1);
+//     })
+    const result = {};
+    participant.forEach(e=> {
+        if(result[e]) result[e] =  result[e] + 1
+        else result[e] = 1;
+    })
+    completion.forEach(e=> {
+        if(result[e]) result[e] =  result[e] - 1
+    })
+    
+    return Object.entries(result).filter(e => e[1] > 0)[0][0]
+    
+//     const result = [...participant];
+    
+//     completion.forEach((c)=> {
+//         const f = result.findIndex(x => x === c);
+//         if(f > -1) result.splice(f, 1);
+//     });
+    
+//     return result[0];
 }
